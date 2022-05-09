a5d1f5cd7037d2fc3274fc60f36e1819e6799396
galeb <ghaleb.butto99@gmail.com>
2022-05-09 14:59:50 +0300

    Q7

--- plan_graph_level.py
+++ plan_graph_level.py
@@ -61,6 +61,7 @@ class PlanGraphLevel(object):
         all_actions = PlanGraphLevel.actions
         "*** YOUR CODE HERE ***"
 
+
     def update_mutex_actions(self, previous_layer_mutex_proposition):
         """
         Updates the mutex set in self.action_layer,
@@ -103,6 +104,10 @@ class PlanGraphLevel(object):
         current_layer_propositions = self.proposition_layer.get_propositions()
         current_layer_mutex_actions = self.action_layer.get_mutex_actions()
         "*** YOUR CODE HERE ***"
+        for p1 in current_layer_propositions:
+            for p2 in current_layer_propositions:
+                if p1 != p2 and mutex_propositions(p1, p2, current_layer_mutex_actions):
+                    self.proposition_layer.add_mutex_prop(p1, p2)
 
     def expand(self, previous_layer):
         """
@@ -149,7 +154,7 @@ def have_competing_needs(a1: Action, a2: Action, mutex_props):
     "*** YOUR CODE HERE ***"
     for single_a1_pre in a1.get_pre():
         for single_a2_pre in a2.get_pre():
-            if mutex_props(Pair(single_a1_pre,single_a2_pre)):
+            if mutex_props(Pair(single_a1_pre, single_a2_pre)):
                 return True
     return False
 
@@ -167,8 +172,6 @@ def mutex_propositions(prop1, prop2, mutex_actions_list):
     action2 = prop2.get_producers()
     for a1 in action1:
         for a2 in action2:
-            if Pair(a1, a2) not in mutex_actions_list and Pair(a2, a1) not in mutex_actions_list: # todo may need only
-                # one comper
+            if Pair(a1, a2) not in mutex_actions_list:
                 return False
     return True
-
