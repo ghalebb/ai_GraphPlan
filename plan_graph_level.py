0634397160c514984dc7d834ffb53b7f265a2fc5
daniel.vishna <daniel.vishna@mail.huji.ac.il>
2022-05-12 12:28:48 +0300

    q11

--- plan_graph_level.py
+++ plan_graph_level.py
@@ -158,7 +158,13 @@ class PlanGraphLevel(object):
         You don't have to use this function
         """
         previous_layer_proposition = previous_layer.get_proposition_layer()
+        previous_layer_mutex_proposition = {}
+
         "*** YOUR CODE HERE ***"
+        self.update_action_layer(previous_layer_proposition)
+        self.update_mutex_actions(previous_layer_mutex_proposition)
+        self.update_proposition_layer()
+        self.update_mutex_proposition()
 
 
 def mutex_actions(a1, a2, mutex_props):
